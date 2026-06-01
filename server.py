c8dc81010dd0add3ea44d04dd1b634ca7277b57f
4sylum00 <4sylum00@mail.com>
2026-06-01 13:27:22 +0200

    A

--- server.py
+++ server.py
@@ -111,5 +111,5 @@ def home():
     """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
+    port = int(os.environ.get("PORT", 7000))
     app.run(host="0.0.0.0", port=port, debug=False)
